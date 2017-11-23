c734f106cbddb0728a053f4165bcea73a9f85d6c
okumuram <okuchan09>
2017-11-23 16:16:09 +0900

    第7,8章完了

--- src/main/java/money/Money.java
+++ src/main/java/money/Money.java
@@ -1,10 +1,19 @@
 package money;
 
-public class Money {
+abstract public class Money {
     protected int amount;
+    abstract Money times(int multiplier);
 
     public boolean equals(Object obj) {
         Money money = (Money) obj;
-        return amount == money.amount;
+        return amount == money.amount && getClass().equals(money.getClass());
+    }
+
+    static Money dollar(int amount) {
+        return new Dollar(amount);
+    }
+
+    static Franc franc(int amount) {
+        return new Franc(amount);
     }
 }
