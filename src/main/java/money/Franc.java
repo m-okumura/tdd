b8712cd6c82cd8cc45ca7827e0cd6f740d5be3c7
okumuram <okuchan09>
2017-11-23 15:40:32 +0900

    第5章完了

---
+++ src/main/java/money/Franc.java
@@ -0,0 +1,19 @@
+package money;
+
+public class Franc {
+    private int amount;
+
+    public Franc(int amount) {
+        this.amount = amount;
+    }
+
+    Franc times(int multiplier) {
+        return new Franc(amount * multiplier);
+    }
+
+    public boolean equals(Object obj) {
+        Franc franc = (Franc) obj;
+        return amount == franc.amount;
+    }
+
+}
