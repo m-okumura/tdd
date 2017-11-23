382f8a696ce257861c927ca4565d3a5d782dc73b
okumuram <okuchan09>
2017-11-23 15:31:28 +0900

    テスト駆動開発　第5章まで写経

---
+++ src/main/java/MoneyTest.java
@@ -0,0 +1,25 @@
+package money;
+
+
+import money.Dollar;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+public class MoneyTest {
+
+    @Test
+    public void testMultiplication() {
+        Dollar five = new Dollar(5);
+        assertEquals(new Dollar(10), five.times(2));
+        assertEquals(new Dollar(15), five.times(3));
+    }
+
+    @Test
+    public void  testEquality() {
+        assertTrue(new Dollar(5).equals(new Dollar(5)));
+        assertFalse(new Dollar(5).equals(new Dollar(6)));
+    }
+}
---
+++ src/main/java/money/Dollar.java
@@ -0,0 +1,21 @@
+package money;
+
+public class Dollar {
+    private int amount;
+
+    public Dollar(int amount) {
+        this.amount = amount;
+    }
+
+    Dollar times(int multiplier) {
+        return new Dollar(amount * multiplier);
+    }
+
+    public boolean equals(Object obj) {
+        Dollar dollar = (Dollar) obj;
+        return amount == dollar.amount;
+    }
+
+
+}
+
